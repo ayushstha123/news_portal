43a5f389efaca71ed42fd978a71c3c33c46f4cbf
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2024-02-03 19:52:28 +0545

    create Header components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,11 @@ import SignIn from './pages/SignIn'
 import About from './pages/About'
 import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
+import Header from './components/Header'
 const App = () => {
   return (
  <BrowserRouter>
+ <Header/>
  <Routes>
   <Route path="/" element={<Home/>} />
   <Route path="/about" element={<About/>} />
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,35 @@
+import { Button, Navbar, NavbarToggle, TextInput } from 'flowbite-react'
+import React from 'react'
+import { Link,useLocation } from 'react-router-dom'
+
+const Header = () => {
+    const path=useLocation().pathname
+  return (
+    <div>
+        <Navbar className='border-b-2 p-2 justify-between '>
+            <div className='flex'>
+            <Link to="/" className='self-center text-sm sm:text-xl font-bold dark:text-white'>
+                News_Portal
+            </Link>
+            <form>
+            <TextInput
+            type='text'
+            placeholder='Search...'
+            className='px-5'
+           />
+        </form> 
+        </div>
+       
+        <Navbar.Toggle/>
+        <Navbar.Collapse>
+            <Navbar.Link active={path==='/'}><Link to='/'>Home</Link></Navbar.Link>
+            <Navbar.Link active={path==='/about'}><Link to='/about'>About</Link></Navbar.Link>
+            <Navbar.Link active={path==='/sigin'}><Link to='/signin'>SignIn</Link></Navbar.Link>
+            </Navbar.Collapse>       
+        </Navbar>
+        
+    </div>
+  )
+}
+
+export default Header
