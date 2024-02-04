cc25a1070fd885045fec27546d4c5758f51d325b
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2024-02-04 09:44:20 +0545

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app=express();
 const PORT=3000;
 
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log('database connected');
+}).catch((error)=>{
+    console.log(error);
+})
 app.listen(PORT,()=>{
     console.log(`server running on port: ${PORT}`);
 })
